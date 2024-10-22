56fedb87d6743c250c2b2e24fc04d32dc9b23516
ankit <ankitaur2022@gmail.com>
2024-10-22 20:15:53 +0530

    Your commit message here

---
+++ python.py
@@ -0,0 +1,19 @@
+# ------------------------------------------Python----------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------
+# print("Hello world")
+
+# name = input("What is your name? ")
+# print("Hello", name)
+
+# lst = ["Rohan Singh" ,"34","Bhubeneswar"]
+# print("My name is {0} and my age is {1}, I live in {2}".format(*lst))
+
+# work as matrix
+# lst1 = ["Rohan Singh" ,"34","Bhubeneswar"]
+# lst2 = ["Aman" ,"32","Delhi"]
+# print("My name is {0[0]} and my age is {0[1]}. I live in {0[2]}wheras my friend's name is {1[0]} and his age is {1[1]}.He lives in {1[2]}".format(
+# lst1,lst2))
+
+# num1 = float(input("Enter the 1st number: "))
+# num2 = float(input("Enter the 2nd number: "))
+# print("The sum of the numbers is", num1+num2)
